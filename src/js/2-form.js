09e7d97fcf0c6384fe4d8c469b263ef3bcd1a8b8
damirmamian <damirmamian@gmail.com>
2025-11-11 17:13:40 +0200

    2nd task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,49 @@
-const formData = {
+const form = document.querySelector(".feedback-form");
+let formData = {
     email: "",
     message: ""
+};
+
+const savedData = localStorage.getItem("feedback-form-state");
+if (savedData) {
+    const parsedData = JSON.parse(savedData) || {};
+
+    formData.email = parsedData.email?.trim() || "";
+    formData.message = parsedData.message?.trim() || "";
+
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
 }
-const form = document.querySelector(".feedback-form")
+
+function FormInput(event) {
+    const { name, value } = event.target;
+
+    if (name !== "email" && name !== "message") {
+        return;
+    }
+
+    formData[name] = value.trim();
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+
+    console.log("Логування при введенні:", formData);
+}
+
+
+function FormSubmit(event) {
+    event.preventDefault();
+
+    if (!formData.email || !formData.message) {
+        alert("Fill in all fields");
+        return;
+    }
+
+    console.log("Об'єкт formData при відправцi:", formData);
+
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+
+    formData = { email: "", message: "" };
+}
+
+form.addEventListener("input", FormInput);
+form.addEventListener("submit", FormSubmit);
